c6ddd8ce9a833cdb85feff5cd887fe25a97bed43
Douglas S. Green <douglas@nurd.site>
2024-06-19 05:25:17 -0400

    test: adding test file

---
+++ bin/test.php
@@ -0,0 +1,28 @@
+<?php
+use PhpParser\Error;
+use PhpParser\NodeDumper;
+use PhpParser\ParserFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$code = <<<'CODE'
+<?php
+
+namespace ABC;
+
+function test($foo)
+{
+    var_dump($foo);
+}
+CODE;
+
+$parser = (new ParserFactory())->createForNewestSupportedVersion();
+try {
+    $ast = $parser->parse($code);
+} catch (Error $error) {
+    echo "Parse error: {$error->getMessage()}\n";
+    return;
+}
+
+$dumper = new NodeDumper;
+echo $dumper->dump($ast) . "\n";
