c3cf8338e5bdb6685f4874063aca748ce70ce1b5
Douglas S. Green <douglas@nurd.site>
2024-06-19 19:08:01 -0400

    feat: adding element visitor

---
+++ bin/lint.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use DouglasGreen\PhpLinter\ElementVisitor;
+use DouglasGreen\Utility\FileSystem\DirUtil;
+use DouglasGreen\Utility\FileSystem\Path;
+use DouglasGreen\Utility\FileSystem\PathUtil;
+use PhpParser\Error;
+use PhpParser\NodeTraverser;
+use PhpParser\ParserFactory;
+
+$parser = (new ParserFactory())->createForNewestSupportedVersion();
+
+$path = new Path();
+$path->addSubpath('php_paths');
+if (! $path->exists()) {
+    die('PHP path file not found in current directory' . PHP_EOL);
+}
+
+$phpPaths = $path->loadLines(Path::IGNORE_NEW_LINES);
+foreach ($phpPaths as $phpPath) {
+    $files = DirUtil::listFiles($phpPath);
+    foreach ($files as $file) {
+        echo '===> ' . $file . PHP_EOL;
+        try {
+            $code = PathUtil::loadString($file);
+            $stmts = $parser->parse($code);
+            if ($stmts === null) {
+                echo 'No statements found in file.' . PHP_EOL;
+                continue;
+            }
+
+            $traverser = new NodeTraverser();
+            $traverser->addVisitor(new ElementVisitor());
+            $traverser->traverse($stmts);
+        } catch (Error $e) {
+            echo 'Parse Error: ', $e->getMessage();
+        }
+    }
+}
--- src/ElementVisitor.php
+++ src/ElementVisitor.php
@@ -1,27 +1,85 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DouglasGreen\PhpLinter;
 
-use PhpParser\Error;
+use DouglasGreen\Utility\Regex\Regex;
 use PhpParser\Node;
-use PhpParser\NodeTraverser;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
+use PhpParser\Node\Stmt\Interface_;
+use PhpParser\Node\Stmt\Namespace_;
+use PhpParser\Node\Stmt\Trait_;
 use PhpParser\NodeVisitorAbstract;
-use PhpParser\ParserFactory;
 
-class ElementVisitor extends NodeVisitorAbstract {
-    public function enterNode(Node $node) {
-        if ($node instanceof Node\Stmt\Namespace_) {
-            echo 'Namespace: ' . $node->name . PHP_EOL;
-        } elseif ($node instanceof Node\Stmt\Class_) {
-            echo 'Class: ' . $node->name . PHP_EOL;
-        } elseif ($node instanceof Node\Stmt\Interface_) {
-            echo 'Interface: ' . $node->name . PHP_EOL;
-        } elseif ($node instanceof Node\Stmt\Trait_) {
-            echo 'Trait: ' . $node->name . PHP_EOL;
-        } elseif ($node instanceof Node\Stmt\ClassMethod) {
-            echo 'Method: ' . $node->name . PHP_EOL;
-        } elseif ($node instanceof Node\Stmt\Function_) {
-            echo 'Function: ' . $node->name . PHP_EOL;
+class ElementVisitor extends NodeVisitorAbstract
+{
+    public function enterNode(Node $node): Node|int|null
+    {
+        $name = property_exists($node, 'name') ? (string) $node->name : '';
+        if ($node instanceof Namespace_) {
+            // echo 'Namespace: ' . $name . PHP_EOL;
+            $parts = explode('\\', $name);
+            foreach ($parts as $part) {
+                if (! $this->checkUpperName($part)) {
+                    break;
+                }
+            }
+        } elseif ($node instanceof Class_) {
+            // echo 'Class: ' . $name . PHP_EOL;
+            $this->checkUpperName($name);
+        } elseif ($node instanceof Interface_) {
+            // echo 'Interface: ' . $name . PHP_EOL;
+            $this->checkUpperName($name);
+        } elseif ($node instanceof Trait_) {
+            // echo 'Trait: ' . $name . PHP_EOL;
+            $this->checkUpperName($name);
+        } elseif ($node instanceof ClassMethod) {
+            // echo 'Method: ' . $name . PHP_EOL;
+            $this->checkLowerName($name);
+        } elseif ($node instanceof Function_) {
+            // echo 'Function: ' . $name . PHP_EOL;
+            $this->checkLowerName($name);
+        } elseif ($node instanceof Variable) {
+            // echo 'Variable: ' . $name . PHP_EOL;
+            $this->checkLowerName($name);
+        } elseif ($name !== '') {
+            //var_dump(get_class($node), $name);
         }
+
+        return null;
+    }
+
+    protected function checkUpperName(string $name): bool
+    {
+        if (! $this->isUpperCamelCase($name)) {
+            echo 'Not camel case: ' . $name . PHP_EOL;
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function checkLowerName(string $name): bool
+    {
+        if (! $this->isLowerCamelCase($name)) {
+            echo 'Not camel case: ' . $name . PHP_EOL;
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function isLowerCamelCase(string $name): bool
+    {
+        return ! Regex::hasMatch('/\$[A-Z]|^[A-Z]|[A-Z]{2}|_/', $name);
+    }
+
+    protected function isUpperCamelCase(string $name): bool
+    {
+        return ! Regex::hasMatch('/[A-Z]{2}|_/', $name);
     }
 }
