885bf254aeb946878f5c4eea9b28930ab2fec075
Douglas S. Green <douglas@nurd.site>
2024-06-25 14:33:35 -0400

    fix: checking var name without $

--- src/Nikic/NameChecker.php
+++ src/Nikic/NameChecker.php
@@ -43,18 +43,18 @@ class NameChecker extends BaseChecker
      * @var list<string>
      */
     protected const SUPERGLOBALS = [
-        '$GLOBALS',
-        '$_SERVER',
-        '$_GET',
-        '$_POST',
-        '$_FILES',
-        '$_REQUEST',
-        '$_SESSION',
-        '$_ENV',
-        '$_COOKIE',
-        '$http_response_header',
-        '$argc',
-        '$argv',
+        'GLOBALS',
+        '_SERVER',
+        '_GET',
+        '_POST',
+        '_FILES',
+        '_REQUEST',
+        '_SESSION',
+        '_ENV',
+        '_COOKIE',
+        'http_response_header',
+        'argc',
+        'argv',
     ];
 
     /**
